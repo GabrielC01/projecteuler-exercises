5f5f23aae4b27ed22726be60a013b749fdfbcfe5
GabrielC01 <gabcchaves@zohomail.com>
2022-01-18 11:44:22 -0300

    cli arg

--- solutions.js
+++ solutions.js
@@ -1,15 +1,26 @@
+import { argv } from 'process';
 // Each function correspond to the solution of a problem
 
+var solutions = [];
+
 // Problem 1: Multiples of 3 and 5
-function p1() {
+solutions.push(function p1() {
 	let sum = 0;
 	for (let i = 3; i < 1000; i++) {
 		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 	}
-	console.log(sum);
-}
+	return sum;
+});
 
-module.exports = {
-	p1
-};
+if (argv.length <= 2) {
+	console.log('Please, inform the number that corresponds to the problem.');
+	console.log('Check the list of problems on projecteuler.net');
+	process.exit(0);
+} else {
+	try {
+		console.log('Solution: ' + solutions[argv[2]-1]());
+	} catch (e) {
+		console.log('Solution not found');
+	}
+}
